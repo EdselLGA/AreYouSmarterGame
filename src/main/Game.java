f38b4228d0461d7a84de91283c685e37445f6e61
Ctr_me <aguilarleighton@gmail.com>
2025-09-17 16:05:26 +0800

    Initial commit

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,7 +1,41 @@
 // Main entry point
+import javax.swing.*;
 public class Game {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Game!");
-        // Initialize game components here
+
+        SwingUtilities.invokeLater(() -> {
+            new Game().start();
+        
+        });
+
     }
+
+    private JFrame mainFrame;
+    //private Player player;
+    //private QuestionManager questionManager;
+
+    public void Game() {
+        //player = new Player ("Player1");
+        //questionManager = new QuestionManager();
+    }
+
+    public void start() {
+        ShowMainmenu();
+    }
+
+    private void ShowMainmenu() {
+        mainFrame = new JFrame("A million bytes");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1000, 700);
+
+        // MainMenuPanel mainMenuPanel = new MainMenuPanel(this);
+        // mainFrame.setContentPane(mainMenuPanel);
+
+        mainFrame.setVisible(true);
+
+    }
+
+    
+   
 }
+
