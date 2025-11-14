ae528cd282cbaefd1e274aa9f22a2011c4ffd87c
Ctr_me <aguilarleighton@gmail.com>
2025-11-14 20:59:04 +0800

    new file:   assets/Cloud.png 	modified:   src/ui/SplashScreenPanel.java

--- src/ui/SplashScreenPanel.java
+++ src/ui/SplashScreenPanel.java
@@ -11,6 +11,7 @@ public class SplashScreenPanel extends JPanel {
     private Timer titlePulseTimer;
     private volatile boolean started = false;
 
+    // paths
     private static final String BACKGROUND =
             "c:\\Users\\edsel\\AreYouSmarterGame-5\\assets\\splash.png";
 
@@ -20,66 +21,85 @@ public class SplashScreenPanel extends JPanel {
     private static final String TITLE_IMAGE =
             "c:\\Users\\edsel\\AreYouSmarterGame-1\\assets\\title.png";
 
+    private static final String CLOUD_IMAGE =
+            "c:\\Users\\edsel\\AreYouSmarterGame-1\\assets\\Cloud.png";
+
     private Image bgImage;
 
     public SplashScreenPanel(GameWindow parent) {
         this.parent = parent;
-
-        setOpaque(false);
-        setLayout(new GridBagLayout());
+        setLayout(new BorderLayout());   
 
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 
-        // -------------------------
-        // Load background
-        // -------------------------
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(screen);
+        layeredPane.setBounds(0, 0, screen.width, screen.height);
+
+        add(layeredPane, BorderLayout.CENTER);
+
         ImageIcon bgIc = new ImageIcon(BACKGROUND);
         if (bgIc.getIconWidth() > 0)
-            bgImage = bgIc.getImage().getScaledInstance(
-                    screen.width, screen.height, Image.SCALE_SMOOTH);
+            bgImage = bgIc.getImage().getScaledInstance(screen.width, screen.height, Image.SCALE_SMOOTH);
 
-        // -------------------------
-        // TITLE IMAGE + ANIMATION
-        // -------------------------
+        JLabel bgLabel = new JLabel(new ImageIcon(bgImage));
+        bgLabel.setBounds(0, 0, screen.width, screen.height);
+        layeredPane.add(bgLabel, Integer.valueOf(0));
+
+        ImageIcon cloudIc = new ImageIcon(CLOUD_IMAGE);
+        int cloudW = (int) (screen.width * 0.25);
+        int cloudH = (int) ((double) cloudIc.getIconHeight() / cloudIc.getIconWidth() * cloudW);
+        Image cloudScaled = cloudIc.getImage().getScaledInstance(cloudW, cloudH, Image.SCALE_SMOOTH);
+
+        JLabel cloudLeft = new JLabel(new ImageIcon(cloudScaled));
+        cloudLeft.setBounds(30, 20, cloudW, cloudH);
+
+        JLabel cloudRight = new JLabel(new ImageIcon(cloudScaled));
+        cloudRight.setBounds(screen.width - cloudW - 30, 20, cloudW, cloudH);
+
+        layeredPane.add(cloudLeft, Integer.valueOf(1));
+        layeredPane.add(cloudRight, Integer.valueOf(1));
+
+        JPanel content = new JPanel();
+        content.setOpaque(false);
+        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+
         JLabel titleLabel = new JLabel();
         ImageIcon titleIc = new ImageIcon(TITLE_IMAGE);
 
         int titleW = (int) (screen.width * 0.90);
         int titleH = (int) ((double) titleIc.getIconHeight() / titleIc.getIconWidth() * titleW);
 
-        Image titleBaseImage = titleIc.getImage()
-                .getScaledInstance(titleW, titleH, Image.SCALE_SMOOTH);
+        Image titleBaseImage =
+                titleIc.getImage().getScaledInstance(titleW, titleH, Image.SCALE_SMOOTH);
 
         titleLabel.setIcon(new ImageIcon(titleBaseImage));
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // TITLE ANIMATION (Pulse)
         titlePulseTimer = new Timer(60, null);
-
         final float[] scale = {1.0f};
         final boolean[] shrinking = {true};
 
         titlePulseTimer.addActionListener(e -> {
             if (shrinking[0]) {
-                scale[0] -= 0.005f;
-                if (scale[0] <= 0.98f) shrinking[0] = false;
+                scale[0] -= 0.004f;
+                if (scale[0] <= 0.985f) shrinking[0] = false;
             } else {
-                scale[0] += 0.005f;
-                if (scale[0] >= 1.02f) shrinking[0] = true;
+                scale[0] += 0.004f;
+                if (scale[0] >= 1.015f) shrinking[0] = true;
             }
 
-            int newW = Math.max(1, (int) (titleW * scale[0]));
-            int newH = Math.max(1, (int) (titleH * scale[0]));
+            int newW = (int) (titleW * scale[0]);
+            int newH = (int) (titleH * scale[0]);
 
-            Image scaled = titleBaseImage.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+            Image scaled =
+                    titleBaseImage.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+
             titleLabel.setIcon(new ImageIcon(scaled));
         });
 
         titlePulseTimer.start();
 
-        // -------------------------
-        // PRESS START
-        // -------------------------
         JLabel pressLabel = new JLabel();
         ImageIcon pressIc = new ImageIcon(PRESS_START);
 
@@ -88,35 +108,19 @@ public class SplashScreenPanel extends JPanel {
             int h = (int) ((double) pressIc.getIconHeight() / pressIc.getIconWidth() * w);
             pressLabel.setIcon(new ImageIcon(
                     pressIc.getImage().getScaledInstance(w, h, Image.SCALE_SMOOTH)));
-        } else {
-            pressLabel.setText("Click or Press Any Key");
-            pressLabel.setFont(new Font("Arial", Font.BOLD, 36));
-            pressLabel.setForeground(Color.WHITE);
         }
-
         pressLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // PRESS START throbbing (already safe)
         throbTimer = HelpersUI.createSimpleTextThrob(pressLabel, 60, 0.98f, 1.02f);
-
-        // -------------------------
-        // Layout container
-        // -------------------------
-        JPanel content = new JPanel();
-        content.setOpaque(false);
-        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 
         content.add(Box.createVerticalGlue());
         content.add(titleLabel);
-        content.add(Box.createRigidArea(new Dimension(0, 40)));
+        content.add(Box.createRigidArea(new Dimension(0, 45)));
         content.add(pressLabel);
         content.add(Box.createVerticalGlue());
 
-        add(content);
-
-        // -------------------------
-        // Input Listeners
-        // -------------------------
+        content.setBounds(0, 0, screen.width, screen.height);
+        layeredPane.add(content, Integer.valueOf(2));
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
@@ -142,9 +146,6 @@ public class SplashScreenPanel extends JPanel {
         new Timer(8000, e -> proceed()).start();
     }
 
-    // -------------------------
-    // Transition to main menu
-    // -------------------------
     private void proceed() {
         if (started) return;
         started = true;
@@ -158,18 +159,8 @@ public class SplashScreenPanel extends JPanel {
         HelpersUI.fadeInComponent(menu, 18, 0.06f, null);
     }
 
-    // -------------------------
-    // Paint background
-    // -------------------------
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        if (bgImage != null)
-            g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
-        else {
-            g.setColor(Color.BLACK);
-            g.fillRect(0,0,getWidth(),getHeight());
-        }
     }
 }
